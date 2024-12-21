5ae137a19fdfa6a0c5c97de84344a5436bb00553
Anezijane <ahnnieanezi@gmail.com>
2024-12-21 23:13:19 +0100

    add useState and handlecgange function in the signUp component

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const uri = process.env.MONGO_URI
+
+
+const app = express()
+
+app.use(express.json()) 
+app.use(cors())
+
+mongoose.connect('', { useNewUrlParser: true, useUnifiedTopology: true }) 
+
+const db = mongoose.connection 
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+    console.log('Connected to database')
+})
