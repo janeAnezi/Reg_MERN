e385ce3914b31ef3d821cd028fa5c582a59e9a05
Anezijane <ahnnieanezi@gmail.com>
2024-12-21 19:57:11 +0100

    add useState and handlecgange function in the signUp component

--- client/src/Component/SignUp.jsx
+++ client/src/Component/SignUp.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios'
 
 const SignUp = () => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
         password: '',
-        confirmPassword: '',
+        // confirmPassword: '',
         error: ''
     })
 
@@ -17,6 +18,9 @@ const SignUp = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        axios.post('',{name, email,password})
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
     }
    
     return (
@@ -64,7 +68,7 @@ const SignUp = () => {
                         onChange={handleChange}
                         />
                     </div>
-                    <div className="flex items-center gap-2 mb-4">
+                    {/* <div className="flex items-center gap-2 mb-4">
                         <label htmlFor="name">
                             <strong>Password</strong>
                         </label>
@@ -76,7 +80,7 @@ const SignUp = () => {
                         required
                         onChange={handleChange}
                         />
-                    </div>
+                    </div> */}
                     <button className="bg-gradient-to-r from-blue-300 to-blue-900 text-white font-bold w-80 px-3 rounded-xl py-1 mb-4" type="submit">Register</button> 
                 </form>
                 <p>Already a member? <Link to='/login' className="text-blue-700 font-semibold">Login</Link></p>
